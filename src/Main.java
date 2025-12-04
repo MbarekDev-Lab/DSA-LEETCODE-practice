6b0be55e3e7e432a0764b11db4507a53aa4a8650
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-12-04 08:31:51 +0100

    DSA + LeetCode practice

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import linkedList.LinkedList;
+import linkedList.Node;
+
+public class Main {
+
+
+    public static void printItems(int a, int b) {
+        for (int i = 0; i < a; i++) {
+//            for (int j = 0; j < a; j++) {
+//                System.out.println("i = " + i + " j " + j + " ");
+//            }
+        }
+
+        for (int k = 0; k < b; k++) {
+            System.out.println("k = " + k);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // printItems(10, 5);
+
+        LinkedList myList = new LinkedList(4);
+        myList.append(10);
+        myList.prepend(1);
+        myList.printList();
+//        System.out.println("Head: " + myList.getHead());
+//        System.out.println("Tail: " + myList.getTail());
+//        System.out.println("Length: " + myList.getLength());
+        // Output:
+//        1 -> 4 -> 10 -> null
+//        Head: 1
+//        Tail: 10
+//        Length: 3
+
+
+        // Create nodes
+        Node head = new Node(11);
+        head.next = new Node(3);
+        head.next.next = new Node(23);
+        head.next.next.next = new Node(7);
+        Node tail = new Node(4);
+
+        head.next.next.next.next = tail;
+
+        // Print the list
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+
+//        1 -> 4 -> 10 -> null
+//        11 -> 3 -> 23 -> 7 -> 4 -> null
+
+//        head = {
+//                "value" = 11,
+//                "next" = {
+//                        "value" = 3,
+//                        "next" = {
+//                                "value" = 23,
+//                                "next" = {
+//                                        "value" = 7,
+//                                        "next" = {
+//                                                "value" = 4,
+//                                                "next" = null
+//                                        }
+//                                }
+//                        }
+//                }
+//        }
+    }
+}
---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,70 @@
+package linkedList;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHead() {
+        return head != null ? head.value : -1;
+    }
+
+    public int getTail() {
+        return tail != null ? tail.value : -1;
+    }
+
+    // --------------------
+    //      APPEND
+    // --------------------
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    // --------------------
+    //      PREPEND
+    // --------------------
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+
+}
